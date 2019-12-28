fd8c3840e96b40c0e632300d8847f700b113612b
Trey <Admin@the-mac-2.home>
2019-12-28 08:20:26 -0600

    Updates for daily work

--- validfloat.sh
+++ validfloat.sh
@@ -20,20 +20,22 @@ validfloat()
 	
 	# check wheter the input number has a decimal point.
 	if [ ! -z $(echo $fvalue | sed 's/[^.]//g') ] ; then
-		
+		echo 1
 		# Extract the part before the decimal point.
 		decimalPart="$(echo $fvalue | cut -d. -f1)"
-		
+		echo 2
 		# Extract the dgigits after the decimal point.
 		fractionalPart="${fvalue#*\.}"
-		
+		echo 3
 		# Start by testing the decimal part, which is everything
 		#   to the left of the decimal point.
 		
-		if [ ! -z $decimalPart ] ;then
+		if [ ! -z $decimalPart ] ; then
 			# "!" reverses test logic, so the following 
 			#   if NOT a valid integer"
+			echo 4
 			if ! validint.sh "$decimalPart" "" "" ; then
+				echo 5
 				return 1
 			fi
 		fi
@@ -42,35 +44,45 @@ validfloat()
 		
 		# to Start, you can't have a negative sign after the decimal point
 		#   like 33.-11, so let's test for the '-' sign in the decimal.
-		if [ "${fractionPart%${fractionalPart#?}}" = "-" ] ; then
-			echo "Invalid floating-point number: '-' not allowed \
-				after decimal point." >&2
+		echo 6.0
+		if [ "${fractionalPart%${fractionalPart#?}}" = "-" ] ; then
+			echo "Invalid floating-point number: '-' not allowed after decimal point." >&2
 			return 1
+			echo 6
 		fi
+		echo 7.0
 		if [ "$fractionalPart" != "" ] ; then
+			echo 7.1
 			# if the fractional part is not a valid integer...
-			if ! validint.sh "$fractionalPart" "0" "" ; then
+			#if ! validint.sh "$fractionalPart" "0" "" ; then --comment out to use other
+			if ! validint "$fractionalPart" "0" "" ; then
 				return 1
+				echo 7
 			fi
 		fi
 	else
 		# If the entire value is just "-", that's not good either.
+		echo 8.0
 		if [ "$fvalue" = "-" ] ; then
 			echo "Invalid floating-point format." >&2
 			return 1
+			echo 8
 		fi
 		
 		# Finally check that the remaining digits are actually
 		#   valid as integers.
+		echo 9.0
 		if ! validint.sh "$fvalue" "" "" ; then
 			return 1
+			echo 9
 		fi 
 	fi
-	
+	echo 10
 	return 0
 }
 
-#if validfloat $1 ; then
-#	echo "$1 is a valid floating-point value."
-#fi
-#exit 0
+if validfloat $1 ; then
+	echo "$1 is a valid floating-point value."
+	echo 11
+fi
+exit 0
